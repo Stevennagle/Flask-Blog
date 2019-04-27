5e1e550c42efb382ae845ea1e8344d4882a6fb31
Steven Nagle <sanagle@ymail.com>
2019-04-26 23:53:46 -0400

    rendering templates - vid 2 ; 7min

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,31 @@
 #test app
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author':'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post',
+        'date_posted':'April 26, 2019'
+    },
+    {
+        'author':'Jane Smith',
+        'title': 'Blog Post 1',
+        'content': 'First post',
+        'date_posted':'April 27, 2019'
+
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
         app.run(debug=True)
