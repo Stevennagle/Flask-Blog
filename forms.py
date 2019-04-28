c102bcd88717ed96a14e473e183e98363d31a745
Steven Nagle <sanagle@ymail.com>
2019-04-28 09:59:39 -0400

    Login and Reg forms - vid 3 10min

--- forms.py
+++ forms.py
@@ -1,7 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 #Need to ensure user-entered text is not a script or command
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username',
+        validators=[DataRequired(), Length(min=2, max=20)])
+
+    email = StringField('Email',
+        validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+        validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+        validators=[DataRequired(), Email()])
+
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField('RememberMe')
+    submit = SubmitField('Login')
+
+
+
