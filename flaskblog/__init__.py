ec0f7a93590a5a4438646931b1c0fdac314b3e28
Steven Nagle <sanagle@ymail.com>
2019-05-02 23:21:05 -0400

    Reorganize package and imports

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy,
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7cd18ff261d5dbe21f54af94fb3a78cc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+from flaskblog import routes
