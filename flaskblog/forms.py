4274d8ce94b846e194ceb710ed15637228e0de66
Steven Nagle <sanagle@ymail.com>
2019-05-05 23:50:49 -0400

    add feature: user can update account info

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -31,4 +32,21 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+        validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
 
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('User already exists. Please log in or choose a different User Name.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already exists. Please log in or choose a different User Name.')
