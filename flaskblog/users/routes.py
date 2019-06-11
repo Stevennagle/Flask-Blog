9950671b9952c1dccd38e91348daed147c567f02
Steven Nagle <sanagle@ymail.com>
2019-06-10 21:46:37 -0400

    updating route dependencies

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -12,7 +12,7 @@ users = Blueprint('users', __name__)
 @users.route("/register", methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form =RegistrationForm()
     if form.validate_on_submit():
         hash_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -20,20 +20,20 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created. Please log in.', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
 
 @users.route("/login", methods=['GET','POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form =LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
         else:
             flash('Login Failed. Please check email and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
